274c712997cb07e5300ea9867a4d658697d9518b
Yunus Emre Celik <yunusemrecelikk21@gmail.com>
2023-06-29 19:16:50 +0300

    server.js update

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('Server running on port ' + port);
 })
