8e1dfc73c1eac68d09ca5f88b73b5fc8af5df066
celikyunusemre <emreemre.ye34@gmail.com>
2023-11-26 21:53:22 +0300

    maybe a solution to suggestions field

--- src/components/Suggestions.svelte
+++ src/components/Suggestions.svelte
@@ -47,47 +47,41 @@
     onMount(search);
 </script>
 
-{#if $streamData.length > 0}
-    <div class="text-center flex flex-col justify-center items-center">
-        <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
-            >Suggestions</P
-        >
-    </div>
-
-    <Card
-        padding="none"
-        size="xl"
-        class="md:container md:mx-auto content-center my-8 grid md:grid-cols-2 gap-4 flex-wrap"
+<div class="text-center flex flex-col justify-center items-center">
+    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
+        >Suggestions</P
     >
-        {#each $streamData as data}
-            <figure
-                id={data.name + "-suggestion-stream"}
-                class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700"
-            >
-                <Avatar src={data.img} alt={data.name} class="flex-shrink-0" />
-                <blockquote
-                    class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400"
-                >
-                    <h3
-                        class="text-lg font-semibold text-gray-900 dark:text-white"
-                    >
-                        {data.title}
-                    </h3>
-                    <h4
-                        class="text-lg font-semibold text-gray-900 dark:text-white"
-                    >
-                        {data.name} is streaming {data.game} to {data.viewers} people!
-                    </h4>
+</div>
 
-                    <Button
-                        class="w-fit my-8"
-                        href={data.data[0].url}
-                        target="_blank"
-                    >
-                        Watch now in {data.data[0].quality}
-                    </Button>
-                </blockquote>
-            </figure>
-        {/each}
-    </Card>
-{/if}
+<Card
+    padding="none"
+    size="xl"
+    class="md:container md:mx-auto content-center my-8 grid md:grid-cols-2 gap-4 flex-wrap"
+>
+    {#each $streamData as data}
+        <figure
+            id={data.name + "-suggestion-stream"}
+            class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700"
+        >
+            <Avatar src={data.img} alt={data.name} class="flex-shrink-0" />
+            <blockquote
+                class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400"
+            >
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    {data.title}
+                </h3>
+                <h4 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    {data.name} is streaming {data.game} to {data.viewers} people!
+                </h4>
+
+                <Button
+                    class="w-fit my-8"
+                    href={data.data[0].url}
+                    target="_blank"
+                >
+                    Watch now in {data.data[0].quality}
+                </Button>
+            </blockquote>
+        </figure>
+    {/each}
+</Card>
