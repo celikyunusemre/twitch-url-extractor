3d731a89be425472360242ae9f972aaec95eebda
yunusemrecelikkk <yunusemre.celik@useinsider.com>
2024-03-21 05:49:01 +0300

    idk

--- src/components/TwitchAuth.svelte
+++ src/components/TwitchAuth.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import { storeToken } from '../stores/twitchtoken.store';
+    import { Button } from "flowbite-svelte";
     
 
     const twitchClientID: String = __TWITCH_API_CLIENTID__;
     const twitchSecret: String = __TWITCH_API_CLIENTSECRET__;
-    const redirectURI: String = "https://twitchextractor.vercel.app/";
+    const redirectURI: String = "https://twitchextractor.vercel.app";
+    // const redirectURI: String = "http://localhost:5173";
 
     const authenticate = () => {
         const authUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${twitchClientID}&redirect_uri=${redirectURI}&response_type=token&scope=user:read:follows`;
@@ -20,11 +22,12 @@
     onMount(() => {
         const token = parseTokenFromHash();
         if (token) {
-            storeToken(token); // Pass the token to the store for storage
             console.log(token)
-            // Redirect to a different route or perform other actions
+            storeToken(token);
         }
     });
 </script>
 
-<button on:click={authenticate}>Connect Twitch</button>
+<!-- {#if !parseTokenFromHash()}
+    <Button on:click={authenticate} color="primary">Connect Twitch</Button>
+{/if} -->
